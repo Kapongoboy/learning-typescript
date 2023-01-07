04869515684d58e80145ef0b265a506c865c09ff
Kapongoboy <plamedi.luis11@gmail.com>
2023-01-07 23:33:51 +0200

    additional squares added

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </div>
+  );
 }
