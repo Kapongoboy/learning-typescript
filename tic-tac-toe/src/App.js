14ade29ce6171fd6a3d28bd7aa85709bafde1c9d
Kapongoboy <plamedi.luis11@gmail.com>
2023-01-10 21:54:09 +0200

    move history buttons added

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -32,7 +32,7 @@ function Board({xIsNext, squares, onPlay}) {
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (xIsNext?"X":"O")
+    status = "Next player: " + (xIsNext?"X":"O");
   }
   return (
     <div>
@@ -57,20 +57,39 @@ function Board({xIsNext, squares, onPlay}) {
 }
 
 export default function Game() {
-  const [history, setHistory] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
   const currentSquares = history[history.length - 1];
+
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    //Todo
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #'+move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+    <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+    )
+  })
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
